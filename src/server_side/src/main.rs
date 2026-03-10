0c6b2223e25f7b056583354095c3ec85ad1adbe0
zillion895 <zillion895@gmail.com>
2026-03-10 13:43:37 -0600

    terminal ui

--- src/server_side/src/main.rs
+++ src/server_side/src/main.rs
@@ -8,7 +8,34 @@ use std::sync::{Arc, Mutex};
 
 
 fn main() {
-    //input arc method (arc is a wayt to share data between threads mutex makes it mutable) 
+    //input arc method (arc is a wayt to share data between threads mutex makes it mutable)
+    let quit = false;
+    while(!quit){
+        print!("\x1b[2J");
+        print!("\x1b[48;5;67m");
+        print!("\x1b[H");
+        print!("\x1b[10E");
+        print!("\x1b[");
+        print!("                                       welcome to the server-games terminal for the server");
+        print!("\x1b[10E]");
+        print!("q:______________________________ quit");
+
+
+        //keyboard input
+        if event::poll(std::time::Duration::from_millis(10)).unwrap() {
+            if let Event::Key(key) = event::read().unwrap() {
+                match key.code {
+                    KeyCode::Char('q') => {
+                        quit = true;
+                        break;
+                    }
+                    _ => {}
+                }
+            }
+        }
+        
+    }
+    print!("\x1b[0m]")
     let _ = start_listening();
     game(stream)
     //connection stuff getting mew clien=mts ect
